d27f01bd29a111d3315fcf8579218111665c3f3d
technosap <saptarshi.mohanta@gmail.com>
2021-05-12 14:40:50 +0530

    Update hash.py

--- hash.py
+++ hash.py
@@ -5,10 +5,9 @@ def hash_it(x):
     Utility Function to use a basic hash
     Implemented by Rohit Sahasrabuddhe
     '''
-    if isinstance(x, str):
-        x = int.from_bytes(x.encode("utf-8"),"big")
     return (x*(x+3)) % 42023
 
 def MAC(message,sharedkey):
     hashin = str(message)+bool2str(sharedkey)
+    hashin = int.from_bytes(hashin.encode("utf-8"),"big")
     return str(hash_it(hashin))
